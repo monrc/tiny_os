e5ae776fa1f3ac16b1af8b49efb2e733bccbb449
monrc_linux <monrc@github.com>
2022-02-08 12:58:21 +0800

    系统调用，由直接调用改为消息发送

--- kernel/syscall.asm
+++ kernel/syscall.asm
@@ -3,12 +3,12 @@
 
 
 INT_VECTOR_SYS_CALL equ 0x90
-_NR_get_ticks		equ 0	;要跟 global.c 中 sys_call_table 的定义相对应！
-_NR_write	    	equ 1
+_NR_printx	    equ 0
+_NR_sendrec	    equ 1
 
 ; 导出符号
-global	get_ticks
-global	write
+global	sendrec
+global	printx
 
 bits 32
 [section .text]
@@ -16,21 +16,20 @@ bits 32
 ; ====================================================================================
 ;                  sendrec(int function, int src_dest, MESSAGE* msg);
 ; ====================================================================================
-
-; ====================================================================
-;                              get_ticks
-; ====================================================================
-get_ticks:
-	mov eax, _NR_get_ticks
+; Never call sendrec() directly, call send_recv() instead.
+sendrec:
+	mov eax, _NR_sendrec
+	mov ebx, [esp + 4]	; function
+	mov ecx, [esp + 8]	; src_dest
+	mov edx, [esp + 12]	; p_msg
 	int INT_VECTOR_SYS_CALL
 	ret
-	
-; ====================================================================================
-;                          void write(char* buf, int len);
-; ====================================================================================
-write:
-        mov     eax, _NR_write
-        mov     ebx, [esp + 4]
-        mov     ecx, [esp + 8]
-        int     INT_VECTOR_SYS_CALL
-        ret
+
+; ====================================================================
+;                              printx
+; ====================================================================
+printx:
+	mov eax, _NR_printx
+	mov edx, [esp + 4]
+	int INT_VECTOR_SYS_CALL
+	ret
